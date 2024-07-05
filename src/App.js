9ad698196622b103b5b647d937e41c509feda92b
Deepak Chaudhary <deepswast@gmail.com>
2024-07-05 06:34:49 +0545

    k

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from "react"
-import Header from "./components/Header"
-import DisplaySec from "./pages/home/DisplaySec"
-import Tech from "./pages/home/Tech"
-
-import Footer from "./components/Footer"
-import Intro from "./pages/home/Intro"
-
-
 
 const App = () => {
   return (
     <div>
-    <Header />
-    <DisplaySec />
-    <Tech />
-    <Intro/>
-    <Footer />
 
-
+      
     </div>
   )
 }
